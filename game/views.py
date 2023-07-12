d850da5c758dedfe5bb5f5e724002d9de26da2e3
Himank Dave <himank.dave40@gmail.com>
2023-07-12 07:59:03 -0400

    Created questions page template

--- game/views.py
+++ game/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
 
+QUESTION_LIMIT = len("What is the capital of Gujarat? What is the capital of Gujarat? What is the capital of ")
 def home(request):
-    return render(request, 'base.html')
+    return render(request, 'question.html')
