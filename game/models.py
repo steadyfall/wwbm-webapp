806f2a1ccf6c4dcff6d754c18d10f869e5180359
Himank Dave <93311724+steadyfall@users.noreply.github.com>
2023-07-13 09:30:12 -0400

    Created all models required for 'game' app

--- game/models.py
+++ game/models.py
@@ -1,3 +1,220 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from time import strftime
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.utils.crypto import get_random_string
+from django.urls import reverse
 
-# Create your models here.
+class Lifeline(models.Model):
+    date_created = models.DateTimeField(default=timezone.now)
+    name = models.CharField(max_length=81)
+    description = models.TextField(max_length=1000, blank=False, null=False)
+
+    class Meta:
+        verbose_name = "lifeline"
+        verbose_name_plural = "lifelines"
+
+    def __str__(self):
+        return self.name
+
+
+class Level(models.Model):
+    level_number = models.IntegerField(
+        verbose_name="Level",
+        default=0,
+        validators=[MinValueValidator(0), MaxValueValidator(16)],
+    )
+    money = models.IntegerField(
+        verbose_name="Prize Money",
+        default=0,
+        validators=[MinValueValidator(0), MaxValueValidator(10_000_000)],
+    )
+
+    class Meta:
+        verbose_name = "lifeline"
+        verbose_name_plural = "lifelines"
+
+    @classmethod
+    def get_default_pk(cls):
+        level, created = cls.objects.get_or_create(level_number=0)
+        return level.level_number
+
+    def print_money(self):
+        return f"{self.money}"
+
+    def __str__(self):
+        return f"Level {self.level_number}"
+
+class Category(models.Model):
+    date_created = models.DateTimeField(default=timezone.now)
+    name = models.CharField(max_length=81)
+
+    @classmethod
+    def get_default_pk(cls):
+        category, created = cls.objects.get_or_create(name="None")
+        return category.pk
+
+    class Meta:
+        verbose_name = "category"
+        verbose_name_plural = "categories"
+
+    def __str__(self):
+        return self.name
+
+
+class Option(models.Model):
+    date_added = models.DateTimeField(default=timezone.now)
+    text = models.CharField(max_length=250)
+    hits = models.ManyToManyField(
+        User, through="ChosenOption", related_name="users_clicked"
+    )
+
+    @classmethod
+    def get_default_pk(cls):
+        option, created = cls.objects.get_or_create(text="None")
+        return option.pk
+
+    class Meta:
+        verbose_name = "option"
+        verbose_name_plural = "options"
+
+    def __str__(self):
+        return self.text
+
+class ChosenOption(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    option = models.ForeignKey(Option, on_delete=models.CASCADE)
+    date_chosen = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        verbose_name = "chosen option"
+        verbose_name_plural = "chosen options"
+
+    def __str__(self):
+        produce = f'{self.user.__str__()} chose {self.option.__str__()} on {strftime("%d-%m-%Y %I:%M:%S %p", self.date_chosen)} UTC'
+        return produce
+
+
+class Question(models.Model):
+    UNASSIGNED = "UNASSIGNED"
+    EASY = "EASY"
+    MEDIUM = "MEDIUM"
+    HARD = "HARD"
+    difficulty_choices = [
+        (UNASSIGNED, UNASSIGNED.title()),
+        (EASY, EASY.title()),
+        (MEDIUM, MEDIUM.title()),
+        (HARD, HARD.title()),
+    ]
+
+    MULTIPLE = "MULTIPLE"
+    TRUE_FALSE = "TRUEFALSE"
+    question_type_choices = [
+        (MULTIPLE, MULTIPLE.title()),
+        (TRUE_FALSE, "True or False"),
+    ]
+
+    date_added = models.DateTimeField(default=timezone.now)
+    falls_under = models.ManyToManyField(Category, related_name="all_questions")
+    text = models.TextField(max_length=666, blank=False, null=False)
+    correct_option = models.ForeignKey(
+        Option,
+        default=Option.get_default_pk,
+        on_delete=models.SET_DEFAULT,
+        related_name="questions_correct",
+    )
+    incorrect_options = models.ManyToManyField(Option, related_name="related_questions")
+    question_type = models.CharField(
+        max_length=20, choices=question_type_choices, default=MULTIPLE
+    )
+    difficulty = models.CharField(
+        max_length=20, choices=difficulty_choices, default=UNASSIGNED
+    )
+    asked_to = models.ManyToManyField(User, related_name="questions_asked")
+
+    @classmethod
+    def get_default_pk(cls):
+        question, created = cls.objects.get_or_create(
+            text="None",
+            correct_option=Option.get_default_pk(),
+            incorrect_options=[Option.get_default_pk()],
+        )
+        return question.pk
+
+    class Meta:
+        verbose_name = "question"
+        verbose_name_plural = "questions"
+
+    def __str__(self):
+        return f"{self.get_difficulty_display()} question under {', '.join([i.__str__() for i in self.falls_under.all()])}"
+
+
+def get_sentinel_user():
+    return User.objects.get_or_create(username="deleted")[0]
+
+
+class Session(models.Model):
+    session_id = models.CharField(
+        primary_key=True, default=get_random_string(8), editable=False, max_length=8
+    )
+    date_created = models.DateTimeField(default=timezone.now)
+    session_user = models.ForeignKey(
+        User, on_delete=models.SET(get_sentinel_user), related_name="initiated_sessions"
+    )
+    current_level = models.ForeignKey(
+        Level,
+        default=Level.get_default_pk,
+        on_delete=models.SET_DEFAULT,
+        related_name="sessions_currently",
+    )
+    score = models.IntegerField(
+        verbose_name="Money Won",
+        default=0,
+        validators=[MinValueValidator(0), MaxValueValidator(100_000_000)],
+    )
+    current_question = models.ForeignKey(
+        Question,
+        default=Question.get_default_pk,
+        on_delete=models.SET_DEFAULT,
+        related_name="sessions_current_question_in",
+    )
+    questions_asked = models.ManyToManyField(
+        Question, through="QuestionOrder", related_name="sessions_asked_in"
+    )
+    correct_qns = models.ManyToManyField(Question, related_name="sessions_correct_in")
+    wrong_qn = models.ForeignKey(
+        Question,
+        default=Question.get_default_pk,
+        on_delete=models.SET_DEFAULT,
+        related_name="sessions_wrong_in",
+    )
+    lifeline_qns = models.ManyToManyField(
+        Question, related_name="sessions_used_lifeline_in"
+    )
+    left_lifelines = models.ManyToManyField(Lifeline, related_name="sessions_unused")
+    used_lifelines = models.ManyToManyField(Lifeline, related_name="sessions_used")
+
+    """ def get_absolute_url(self):
+        return reverse('session', kwargs={'session_id': self.session_id}) """
+
+    class Meta:
+        verbose_name = "session"
+        verbose_name_plural = "sessions"
+
+    def __str__(self):
+        return f"{self.session_id}"
+
+
+class QuestionOrder(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    session = models.ForeignKey(Session, on_delete=models.CASCADE)
+    date_chosen = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        verbose_name = "ordering of question in session"
+        verbose_name_plural = "ordering of questions in session"
+
+    def __str__(self):
+        produce = f'{self.session.session_id} - {self.question.__str__()} on {strftime("%d-%m-%Y %I:%M:%S %p", self.date_chosen)} UTC'
+        return produce
