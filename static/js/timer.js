ae7d62d00e6541b411b6ef667c31356bf01ab0d7
Himank Dave <93311724+steadyfall@users.noreply.github.com>
2023-07-13 02:15:12 -0400

    Made changes to timer.js for better timer handling

--- static/js/timer.js
+++ static/js/timer.js
@@ -1,26 +1,43 @@
+function setNewDate() {
+    newDate = new Date().getTime() + countDownmicsec;
+    localStorage.setItem('newDate', newDate);
+};
+function setDifference() {
+    difference = newDate - new Date().getTime();
+    localStorage.setItem('difference', difference);
+};
+
+var timeLeft = document.getElementById("time-left");
+
 var countDownSec = 20;
 var countDownmicsec = countDownSec * 1000;
 
-// Get NOW
-var now = new Date();
-var newDate = now.getTime() + countDownmicsec;
+var newDate = localStorage.getItem('newDate');
+if (!newDate) {setNewDate();}
+var difference = localStorage.getItem('difference')
+if (!difference) {setDifference();}
+
+if (newDate - new Date().getTime() <= 0) {
+    localStorage.clear();
+    setNewDate();
+    setDifference();
+    timeLeft.innerHTML = countDownSec;
+} else {
+    timeLeft.innerHTML = Math.floor((newDate - new Date().getTime()) / 1000);
+}
 
 var x = setInterval(function () {
-
-    var now = new Date();
-    // console.log(now.getTime());
-    var difference = newDate - now.getTime();
-    // console.log(difference);
-    var seconds = Math.floor(difference / 1000);
-    // console.log(seconds);
-    var timeLeft = document.getElementById("time-left");
-    if (timeLeft.innerHTML < 10) {
-        timeLeft.style.color = 'red';
-    }
-    timeLeft.innerHTML = seconds;
-    if (difference < 100) {
+    difference = newDate - new Date().getTime();
+    console.log(difference);
+    if (difference < 10) {
         window.location.href = 'https://www.encodedna.com/javascript/operators/default.htm';
         // document.getElementById("newform").submit()
     }
-
-}, 1000);
+    var seconds = Math.floor(difference / 1000);
+    // console.log(seconds);
+    timeLeft = document.getElementById("time-left");
+    if (timeLeft.innerHTML <= 10) {
+        timeLeft.style.color = 'red';
+    }
+    timeLeft.innerHTML = seconds;
+    }, 1000);
