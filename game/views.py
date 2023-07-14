27ab1f8d8470eac234f16d9024480032ae134fcd
Himank Dave <himank.dave40@gmail.com>
2023-07-14 07:10:31 -0400

    Fixed changes to models, made landing page

--- game/views.py
+++ game/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
+from game.models import *
+import random
 
-QUESTION_LIMIT = len("What is the capital of Gujarat? What is the capital of Gujarat? What is the capital of ")
+
+def mainPage(request):
+    return render(request, "mainPage.html")
+
 def home(request):
-    return render(request, 'question.html')
+    allOptions = [i.text for i in Question.objects.all()[345].incorrect_options.all()]
+    allOptions.extend([Question.objects.all()[345].correct_option.text])
+    random.shuffle(allOptions)
+    p1 = allOptions[:2]
+    p2 = allOptions[2:]
+
+    context = {
+        "question": Question.objects.all()[345].text,
+        "options_part1": p1,
+        "options_part2": p2,
+    }
+    return render(request, "question.html", context)
