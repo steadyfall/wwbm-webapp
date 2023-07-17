0d4555dae9476d9afdf8b55852527004d1743415
Himank Dave <93311724+steadyfall@users.noreply.github.com>
2023-07-17 07:58:06 -0400

    Made a basic working version of the game, with no landing page after answering all 15 questions

--- game/urls.py
+++ game/urls.py
@@ -6,7 +6,8 @@ from . import views as game_views
 urlpatterns = [
     path('', game_views.MainPage.as_view(), name="mainpage"),
     path('game/<str:session>/rules/', game_views.Rules.as_view(), name="rules"),
-    path('game/<str:session>/question/<int:question>/', game_views.MainPage.as_view(), name="question"),
+    path('game/<str:session>/question/<int:level>/', game_views.QuestionInGame.as_view(), name="question"),
+    path('game/<str:session>/question/<int:level>/correct', game_views.sample, name="correct")
 ]
 
 if settings.DEBUG:
