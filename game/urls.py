ea1d33427da1809313af9817b4ad502e23bb2392
Himank Dave <93311724+steadyfall@users.noreply.github.com>
2023-07-26 04:21:36 -0400

    made minor changes to Leaderboard view, created and registered Scoreboard view [for logged in users]

--- game/urls.py
+++ game/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('', game_views.MainPage.as_view(), name="mainpage"),
     path('about/', game_views.About.as_view(), name="about"),
     path('leaderboard/', game_views.Leaderboard.as_view(), name="leaderboard"),
+    path('scores/', game_views.ScoreBoard.as_view(), name="scores"),
     path('game/<str:session>/rules/', game_views.Rules.as_view(), name="rules"),
     path('game/<str:session>/question/<int:level>/', game_views.QuestionInGame.as_view(), name="question"),
     path('game/<str:session>/question/<int:level>/<str:status>/', game_views.BetweenQuestion.as_view(), name="statusAfterQn"),
