90d9af74e17fc4280a34736cc5a93ac05cb100c3
Himank Dave <93311724+steadyfall@users.noreply.github.com>
2023-07-26 07:43:50 -0400

    Changed path of Django admin panel from 'admin/' to 'djadmin/'

--- kbc/urls.py
+++ kbc/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('djadmin/', admin.site.urls),
     path('', include('game.urls')),
     path('auth/', include('auth.urls'))
 ]
