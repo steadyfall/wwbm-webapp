2cab6e19b682473eceeee7bd9202be6e94d8834e
Himank Dave <himank.dave40@gmail.com>
2023-07-26 07:45:21 -0400

    made signin and signout views for custom admin panel (inside auth app)

--- auth/urls.py
+++ auth/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('login/', authApp.login, name="login"),
     path('logout/', authApp.logout, name="logout"),
     path('register/', authApp.register, name="register"),
+    path('admin-login/', authApp.adminlogin, name="adminLogin"),
+    path('admin-logout/', authApp.adminlogout, name='adminLogout'),
+
 ]
--- auth/views.py
+++ auth/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, HttpResponseRedirect
+
 from django.contrib import messages
 from django.contrib.auth.models import User
+
 from django.contrib import auth
 from .validate import *
 
@@ -59,7 +61,7 @@ def login(request):
             user = auth.authenticate(username=username, password=password)
             if user is not None:
                 auth.login(request, user)
-                return redirect('mainpage')
+                return redirect("mainpage")
             else:
                 messages.error(request, "Wrong password. Try again.")
                 return redirect("login")
@@ -69,8 +71,55 @@ def login(request):
 
     return render(request, "authentication/signin.html")
 
+
 def logout(request):
     if not request.user.is_authenticated:
-        return redirect('mainpage')
+        return redirect("mainpage")
     auth.logout(request)
     return render(request, "authentication/signout.html")
+
+
+def adminlogin(request):
+    if request.user.is_authenticated:
+        if request.user.is_superuser:
+            redirect("admin_page")
+        else:
+            return redirect("mainpage")
+
+    error: str = "Data is invalid. Try again."
+    if request.method == "POST":
+        try:
+            username: str = request.POST["username"]
+            password: str = request.POST["password"]
+        except KeyError:
+            messages.error(request, error)
+            return redirect("adminLogin")
+
+        check: bool = usernameValidator(username)
+        if check:
+            if not User.objects.filter(username=username).exists():
+                messages.warning(
+                    request, "You don't have an account. Contact the administrator."
+                )
+                return redirect("adminLogin")
+            user = auth.authenticate(username=username, password=password)
+            if user is not None:
+                auth.login(request, user)
+                if user.is_superuser:
+                    return redirect("adminMainPage")
+                return redirect("mainpage")
+            else:
+                messages.error(request, "Wrong password. Try again.")
+                return redirect("adminLogin")
+        else:
+            messages.error(request, error)
+            return redirect("adminLogin")
+
+    return render(request, "authentication/adminlogin.html")
+
+
+def adminlogout(request):
+    if not request.user.is_authenticated or not request.user.is_superuser:
+        return redirect("mainpage")
+    auth.logout(request)
+    return render(request, "authentication/adminlogout.html")
