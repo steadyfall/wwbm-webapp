51acd4383025b309a011affde8cb077b468b9a40
Himank Dave <himank.dave40@gmail.com>
2023-07-27 06:31:04 -0400

    made AdminListDB view, registered it for respective URL and made related changes to listdb.html

--- adminpanel/urls.py
+++ adminpanel/urls.py
@@ -1,7 +1,11 @@
-from django.urls import path, include
+from django.urls import path
 from . import views as adminV
 
 urlpatterns = [
     path("", adminV.testSite, name="adminMainPage"),
     path("test/", adminV.testSite, name="test"),
+    path("apps/<str:db>/", adminV.AdminListDB.as_view(), name="adminListDB"),
+    path("apps/<str:db>/object/<pk>/", adminV.testSite, name="adminDBObject"),
+    path("apps/<str:db>/object/create/", adminV.testSite, name="adminDBObjectCreate"),
+    path("apps/<str:db>/object/<pk>/delete/", adminV.testSite, name="adminDBObjectDelete"),
 ]
