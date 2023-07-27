0062b4a3d96dbcecfb8ce5aef58d1766fd7ee64e
Himank Dave <93311724+steadyfall@users.noreply.github.com>
2023-07-27 04:27:10 -0400

    added required mixins in mixins.py

---
+++ adminpanel/mixins.py
@@ -0,0 +1,8 @@
+from django.contrib.auth.mixins import UserPassesTestMixin
+
+class SuperuserRequiredMixin(UserPassesTestMixin):
+    """
+    Check if the user is a superuser (admin) or not.
+    """
+    def test_func(self):
+        return self.request.user.is_superuser
