1fe31bc416e399aa1d360e23226dae8fa9bce69f
Himank Dave <93311724+steadyfall@users.noreply.github.com>
2023-07-31 06:49:40 -0400

    made changes to custom form for Question model, accomodating for instances and POST data and processing it within templates

--- static/js/adminpanelForms/questionform.js
+++ static/js/adminpanelForms/questionform.js
@@ -1,74 +1,109 @@
-new TomSelect('#who_added', {
-    plugins: {
-        'remove_button': {
-            title: 'Remove this option.',
+var whoAddedConfig = (allOptions) => {
+    return {
+        plugins: {
+            'remove_button': {
+                title: 'Remove this user.',
+            },
+            'no_backspace_delete': {},
         },
-        'no_backspace_delete': {},
-    },
-    maxItems: 1,
-    items: [],
-    maxOptions: 10,
-    openOnFocus: false,
-    placeholder: "Who added this question?",
-    create: false,
-    highlight: true
-});
-new TomSelect('#falls_under', {
-    plugins: {
-        'remove_button': {
-            title: 'Remove this category.',
+        maxItems: 1,
+        items: allOptions,
+        maxOptions: 10,
+        openOnFocus: false,
+        placeholder: "Who added this question?",
+        create: false,
+        highlight: true
+    }
+};
+var fallsUnderConfig = (allOptions) => {
+    return {
+        plugins: {
+            'remove_button': {
+                title: 'Remove this category.',
+            },
+            'no_backspace_delete': {},
         },
-        'no_backspace_delete': {},
-    },
-    maxItems: null,
-    items: [],
-    maxOptions: 10,
-    openOnFocus: false,
-    placeholder: "Choose the categories.",
-    create: false,
-    highlight: true
-});
-new TomSelect('#correct_option', {
-    plugins: {
-        'remove_button': {
-            title: 'Remove this option.',
+        maxItems: null,
+        items: allOptions,
+        maxOptions: 10,
+        openOnFocus: false,
+        placeholder: "Choose the categories.",
+        create: false,
+        highlight: true
+    }
+};
+var questionTypeConfig = (allOptions) => {
+    return {
+        plugins: {
+            'remove_button': {
+                title: 'Remove this type.',
+            },
+            'no_backspace_delete': {},
         },
-        'no_backspace_delete': {},
-    },
-    maxItems: 1,
-    items: [],
-    maxOptions: 60,
-    openOnFocus: false,
-    placeholder: "Choose a correct option.",
-    create: false,
-    highlight: true
-});
-new TomSelect('#incorrect_options', {
-    plugins: {
-        'remove_button': {
-            title: 'Remove this option.',
+        maxItems: 1,
+        items: allOptions,
+        maxOptions: 10,
+        openOnFocus: false,
+        placeholder: "Which type is the given question?",
+        create: false,
+        highlight: true
+    }
+};
+var difficultyConfig = (allOptions) => {
+    return {
+        plugins: {
+            'remove_button': {
+                title: 'Remove this difficulty.',
+            },
+            'no_backspace_delete': {},
         },
-        'no_backspace_delete': {},
-        'input_autogrow': {},
-    },
-    maxItems: 3,
-    items: [],
-    maxOptions: 95,
-    openOnFocus: false,
-    placeholder: "Choose incorrect options.",
-    create: false,
-    highlight: true
-});
-new flatpickr("#date_added", {
+        maxItems: 1,
+        items: allOptions,
+        maxOptions: 10,
+        openOnFocus: false,
+        placeholder: "What difficulty is this question?",
+        create: false,
+        highlight: true
+    }
+};
+var correctOptionConfig = (allOptions) => {
+    return {
+        plugins: {
+            'remove_button': {
+                title: 'Remove this option.',
+            },
+            'no_backspace_delete': {},
+        },
+        maxItems: 1,
+        items: allOptions,
+        maxOptions: 60,
+        openOnFocus: false,
+        placeholder: "Choose a correct option.",
+        create: false,
+        highlight: true
+    }
+};
+var incorrectOptionConfig = (allOptions) => {
+    return {
+        plugins: {
+            'remove_button': {
+                title: 'Remove this option.',
+            },
+            'no_backspace_delete': {},
+            'input_autogrow': {},
+        },
+        maxItems: 3,
+        items: allOptions,
+        maxOptions: 95,
+        openOnFocus: false,
+        placeholder: "Choose incorrect options.",
+        create: false,
+        highlight: true
+    }
+};
+const dateAddedConfig = {
     enableTime: true,
-    dateFormat: "Y-m-d h:i K",
-    time_24hr: false,
-});
-$(document).ready(function () {
-    $(window).keydown(function (event) {
-        if (event.keyCode == 13) {
-            event.preventDefault();
-            return false;
-        }
-    });
-});
+    dateFormat: "Y-m-dTH:i",
+    time_24hr: true,
+};
+export { whoAddedConfig, fallsUnderConfig, correctOptionConfig, incorrectOptionConfig, questionTypeConfig, difficultyConfig, dateAddedConfig };
