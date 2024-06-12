b593036a25c86071f4e9d8f07cfb5296bd51d279
Himank Dave <himank.dave40@gmail.com>
2024-06-12 19:00:26 -0400

    chore: update settings.py to accomodate dev and prod settings

--- kbc/wsgi.py
+++ kbc/wsgi.py
@@ -1,16 +1,8 @@
-"""
-WSGI config for kbc project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
-"""
-
 import os
 
-from django.core.wsgi import get_wsgi_application
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kbc.settings")
+os.environ.setdefault("DJANGO_CONFIGURATION", "Dev")
+
+from configurations.wsgi import get_wsgi_application
 
 application = get_wsgi_application()
