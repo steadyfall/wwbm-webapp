083d7db1552d428efe8741d6011abe7b7cf3391c
Himank Dave <hddave@uwaterloo.ca>
2024-10-18 04:22:32 -0400

    fix: moved category_lookup to common functions & renamed _fetchqns to _private

--- game/management/commands/_private.py
+++ game/management/commands/_private.py
@@ -1,4 +1,5 @@
 import asyncio, requests, itertools
+from html import unescape
 
 difficulty_choices = list(
     (
@@ -18,3 +19,14 @@ def html_get_sync(link):
 
 async def html_get(link):
     return await asyncio.to_thread(html_get_sync, link)
+
+async def category_lookup():
+    categories = {}
+    link = "https://opentdb.com/api_category.php"
+    result = await html_get(link)
+    if len(result['trivia_categories']) == 0:
+        print(" | ".join([link, 'Not Done']))
+        return None
+    for cat in result['trivia_categories']:
+        categories[int(cat['id'])] = unescape(cat['name'])
+    return categories
